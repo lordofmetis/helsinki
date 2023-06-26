993088339ac6f433fb7f4db4a41a09b06b18b105
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-06-26 15:03:37 +0800

    update a file

--- part06-04_course_grading_part_1/src/course_grading_part_1.py
+++ part06-04_course_grading_part_1/src/course_grading_part_1.py
@@ -1,35 +1,31 @@
 # write your solution here
-if False:
-    # this is never executed
+if True:
     student_info = input("Student information: ")
     exercise_data = input("Exercises completed: ")
 else:
-    # hard-coded input
     student_info = "students1.csv"
     exercise_data = "exercises1.csv"
 
 names = {}
-
 with open(student_info) as left_file:
     for line in left_file:
         parts = line.split(";")
         if parts[0] == "id":
             continue
-        names[parts[0]] = parts[1: ]
+        names[parts[0]] = parts[1] + " " + parts[2].strip()
 
 exercises = {}
-temp = 0
 with open(exercise_data) as right_file:
     for line in right_file:
         parts = line.split(";")
         if parts[0] == "id":
             continue
+        temp = 0
         for part in parts[1: ]:
-            part = int(part)
-            temp += part
+            temp += int(part)
         exercises[parts[0]] = temp
 
 for pic, name in names.items():
     if pic in exercises:
-        exercise = exercises[name]
-        print(f"{name} {exercise}")
+        exercise = exercises[pic]
+    print(f"{name} {exercise}")

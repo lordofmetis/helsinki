3a022e32e9750d450ceb094c9404998bea31fd98
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-06-26 23:04:54 +0800

    Upadate a file temporarily

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,1 +1,10 @@
 # write your solution here
+
+user_input = input("Write text: ")
+parts = user_input.split(" ")
+
+with open("wordlist.txt") as file:
+    for part in parts:
+        if part in file:
+            part = f"*{part}*"
+    print(user_input)

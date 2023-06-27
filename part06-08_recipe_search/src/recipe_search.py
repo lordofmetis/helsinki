ef892a8db9cb7defb5ee2cd6159666da6e6773ba
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-06-27 22:58:57 +0800

    update a file temporarily

--- part06-08_recipe_search/src/recipe_search.py
+++ part06-08_recipe_search/src/recipe_search.py
@@ -1,1 +1,26 @@
-# Write your solution here
+def search_by_name(filename: str, word: str):
+    temp = []
+    final = []
+    word = word.lower()
+
+    with open(filename) as file:
+        for line in file:
+            line = line.strip()
+            temp.append(line)
+
+    in_recipe_name = True
+    for i in temp:
+        if i == "":
+            in_recipe_name = True
+        elif in_recipe_name:
+            if word in i.lower():
+                final.append(i)
+            in_recipe_name = False
+
+    return final
+
+if __name__ == "__main__":
+    found_recipes = search_by_name("recipes1.txt", "cake")
+
+    for recipe in found_recipes:
+        print(recipe)

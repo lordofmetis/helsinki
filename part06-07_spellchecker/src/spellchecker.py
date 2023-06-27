6980ccb254b1da9309b40978f3a8d400509d1751
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-06-27 10:16:21 +0800

    Add files and update a file

--- part06-07_spellchecker/src/spellchecker.py
+++ part06-07_spellchecker/src/spellchecker.py
@@ -1,10 +1,20 @@
 # write your solution here
+if True:
+    user_input = input("Write text: ")
+else:
+    user_input = "I have a gii epsianl"
 
-user_input = input("Write text: ")
 parts = user_input.split(" ")
-
+dictionary = []
 with open("wordlist.txt") as file:
-    for part in parts:
-        if part in file:
-            part = f"*{part}*"
-    print(user_input)
+    for line in file:
+        line = line.strip("\n")
+        dictionary.append(line.lower())
+
+updated_user_input = user_input
+
+for part in parts:
+    if part.lower() not in dictionary:
+        updated_user_input = updated_user_input.replace(part, f"*{part}*")
+
+print(updated_user_input)

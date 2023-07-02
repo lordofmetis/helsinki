885239c23609a8fe82f6fa3a10b1ff73f46df50d
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-02 22:47:59 +0800

    Update files

--- part06-17_read_input/src/read_input.py
+++ part06-17_read_input/src/read_input.py
@@ -1,1 +1,15 @@
 # Write your solution here
+def read_input(user_input: str, lower: int, higher: int):
+    while True:
+        try:
+            number = int(input(user_input))
+            if number > lower and number < higher:
+                return number
+            else:
+                print(f"You must type in an integer between {lower} and {higher}")
+        except ValueError:
+            print(f"You must type in an integer between {lower} and {higher}")
+
+if __name__ == "__main__":
+    number = read_input("Enter a number: ", 5, 10)
+    print("You typed in:", number)
--- part06-18_parameter_validation/src/parameter_validation.py
+++ part06-18_parameter_validation/src/parameter_validation.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def new_person(name: str, age: int):
+    apple = (name, age)
+    if name == "":
+        raise ValueError
+    elif " " not in name:
+        raise ValueError
+    elif len(name) >= 40:
+        raise ValueError
+    elif age < 0 or age >= 150:
+        raise ValueError
+    else:
+        return apple
+
+if __name__ == "__main__":
+    old_person = new_person("James Jameson", 140)
+    print(old_person)

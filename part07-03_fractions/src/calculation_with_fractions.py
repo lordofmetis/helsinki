162a9625cb67d0b1544bd3629e35ec95a9eeff5d
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-03 19:33:16 +0800

    Update a file

--- part07-03_fractions/src/calculation_with_fractions.py
+++ part07-03_fractions/src/calculation_with_fractions.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def fractionate(amount: int):
+    from fractions import Fraction
+
+    empty = []
+
+    for _ in range(amount):
+        empty.append(Fraction(1, amount))
+        
+    return empty
+
+if __name__ == "__main__":
+    print(fractionate(5))

4a5a5a91d0c77f2a0c3664f8d65fd7b4c637860d
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-03 22:03:18 +0800

    Update a file

--- part07-04_lottery_numbers/src/lottery_numbers.py
+++ part07-04_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,16 @@
 # Write your solution here
+def lottery_numbers(amount: int, lower: int, upper: int):
+    from random import sample
+    
+    empty = []
+    
+    numbers = list(range(lower, upper + 1))
+    samplee = sample(numbers, amount)
+
+    samplee = sorted(samplee)
+
+    return samplee
+    
+if __name__ == "__main__":
+    for number in lottery_numbers(1, 1, 10):
+        print(number)

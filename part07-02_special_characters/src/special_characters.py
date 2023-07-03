2d9f1103eb4c62006ec1c1c767b5c417f8e7a46f
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-03 19:16:55 +0800

    Update a file

--- part07-02_special_characters/src/special_characters.py
+++ part07-02_special_characters/src/special_characters.py
@@ -1,1 +1,28 @@
 # Write your solution here
+def separate_characters(my_string: str):
+    from string import ascii_letters, punctuation
+
+    first = ""
+    temp = ""
+    second = ""
+    final = ""
+
+    for i in my_string:
+        if i in ascii_letters:
+            first += i
+        else:
+            temp += i
+    
+    for j in temp:
+        if j in punctuation:
+            second += j
+        else:
+            final += j
+
+    return first, second, final
+
+if __name__ == "__main__":
+    parts = separate_characters("'abc', '', '.!åäö.!åäö'")
+    print(parts[0])
+    print(parts[1])
+    print(parts[2])

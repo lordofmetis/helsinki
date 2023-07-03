d83ecd36ed275b5cabb3d91a6ab220d66b1b9b37
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-03 16:31:11 +0800

    Update a file

--- part07-01_hypotenuse/src/hypotenuse.py
+++ part07-01_hypotenuse/src/hypotenuse.py
@@ -1,1 +1,9 @@
 # Write your solution here
+def hypotenuse(leg1: float, leg2: float):
+    from math import sqrt
+
+    leg3 = sqrt(leg1 ** 2 + leg2 ** 2)
+    return leg3
+
+if __name__ == "__main__":
+    print(hypotenuse(3,4))

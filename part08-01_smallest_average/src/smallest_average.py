1fd53c27b0d2ce3f9aa18f454eff04bc0d0216df
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-05 00:01:27 +0800

    Update a file

--- part08-01_smallest_average/src/smallest_average.py
+++ part08-01_smallest_average/src/smallest_average.py
@@ -1,1 +1,28 @@
 # Write your solution here
+def smallest_average(person1: dict, person2: dict, person3: dict):
+    
+    first = ""
+    second = ""
+    third = ""
+
+    first = (person1["result1"] + person1["result2"] + person1["result3"]) / 3
+
+    second = (person2["result1"] + person2["result2"] + person2["result3"]) / 3
+
+    third = (person3["result1"] + person3["result2"] + person3["result3"]) / 3
+
+    minimum = min(first, second, third)
+    
+    if first == minimum:
+            return person1
+    elif second == minimum:
+        return person2
+    elif third == minimum:
+        return person3
+    
+if __name__ == "__main__":
+    person1 = {"name": "Mary", "result1": 2, "result2": 3, "result3": 3}
+    person2 = {"name": "Gary", "result1": 5, "result2": 1, "result3": 8}
+    person3 = {"name": "Larry", "result1": 3, "result2": 1, "result3": 1}
+
+    print(smallest_average(person1, person2, person3))

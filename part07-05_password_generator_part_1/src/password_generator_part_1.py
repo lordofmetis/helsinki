699892c0724ed9185ea46f6c9ff970166e4f2636
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-04 16:25:08 +0800

    Update a file

--- part07-05_password_generator_part_1/src/password_generator_part_1.py
+++ part07-05_password_generator_part_1/src/password_generator_part_1.py
@@ -1,1 +1,17 @@
 # Write your solution here
+def generate_password(length: int):
+    from random import sample
+    from string import ascii_lowercase
+
+    temp = sample(ascii_lowercase, length)
+
+    empty = ""
+
+    for i in temp:
+        empty += i
+    
+    return empty
+
+if __name__ == "__main__":
+    for i in range(10):
+        print(generate_password(8))

86d5b51b01c28ce4f0fd957cf63147ec165ac28f
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-05 08:25:07 +0800

    Update a file

--- part08-02_row_sums/src/row_sums.py
+++ part08-02_row_sums/src/row_sums.py
@@ -1,1 +1,13 @@
 # Write your solution here
+def row_sums(my_matrix: list):
+
+    for element in my_matrix:
+        temp = 0
+        for i in element:
+            temp += i
+        element.append(temp)
+
+if __name__ == "__main__":
+    my_matrix = [[1,2], [3,4]]
+    row_sums(my_matrix)
+    print(my_matrix)

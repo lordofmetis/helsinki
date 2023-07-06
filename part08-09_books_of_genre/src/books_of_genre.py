0d2ab6d96dfa96ef8d51655b949017bbc2cfe2f9
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-06 11:03:29 +0800

    update files

--- part08-09_books_of_genre/src/books_of_genre.py
+++ part08-09_books_of_genre/src/books_of_genre.py
@@ -12,7 +12,24 @@ class Book:
     def __repr__(self):
         return f"{self.name} ({self.author}), {self.year} - genre: {self.genre}"
 
-
 # -----------------------------
 # Write your solution here
 # -----------------------------
+
+def books_of_genre(books: list, genre: str):
+    temp = []
+    for book in books:
+        if book.genre == genre:
+            temp.append(book)
+    return temp
+
+if __name__ == "__main__":
+    python = Book("Fluent Python", "Luciano Ramalho", "programming", 2015)
+    everest = Book("High Adventure", "Edmund Hillary", "autobiography", 1956)
+    norma = Book("Norma", "Sofi Oksanen", "crime", 2015)
+
+    books = [python, everest, norma, Book("The Snowman", "Jo Nesbø", "crime", 2007)]
+
+    print("Books in the crime genre:")
+    for book in books_of_genre(books, "crime"):
+        print(f"{book.author}: {book.name}")

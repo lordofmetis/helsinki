fad9c3cd1401a98f899d8df00a07fd3f87f0d73b
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-08 22:44:29 +0800

    Add and update files

--- part09-08_shortest_in_room/src/shortest_in_room.py
+++ part09-08_shortest_in_room/src/shortest_in_room.py
@@ -6,3 +6,66 @@ class Person:
 
     def __str__(self):
         return self.name
+
+
+class Room:
+    def __init__(self):
+        self.people = []
+        self.count = 0
+        self.combined_height = 0
+
+    def add(self, person: Person):
+        self.people.append(person)
+        self.count += 1
+        self.combined_height += person.height
+
+    def is_empty(self):
+        if len(self.people) != 0:
+            return False
+        else:
+            return True
+
+    def shortest(self):
+        shortest = [None, 10000]
+        if self.is_empty():
+            return None
+        else:
+            for ren in self.people:
+                if ren.height < shortest[1]:
+                    shortest[1] = ren.height
+                    shortest[0] = ren
+            return shortest[0]
+
+    def remove_shortest(self):
+        if self.is_empty():
+            return None
+        else:
+            shortest = self.shortest()
+            self.people.remove(shortest)
+            return shortest
+
+    def print_contents(self):
+        print(
+            f"There are {self.count} persons in the room, and their combined height is {self.combined_height} cm"
+        )
+        for ren in self.people:
+            print(f"{ren.name} ({ren.height}cm)")
+
+
+if __name__ == "__main__":
+    room = Room()
+
+    room.add(Person("Lea", 183))
+    room.add(Person("Kenya", 172))
+    room.add(Person("Nina", 162))
+    room.add(Person("Ally", 166))
+    room.print_contents()
+
+    print()
+
+    removed = room.remove_shortest()
+    print(f"Removed from room: {removed.name}")
+
+    print()
+
+    room.print_contents()
--- part09-09_car/src/car.py
+++ part09-09_car/src/car.py
@@ -1,1 +1,31 @@
 # WRITE YOUR SOLUTION HERE:
+class Car:
+    def __init__(self):
+        self.__petrol = 0
+        self.__reading = 0
+
+    def fill_up(self):
+        self.__petrol = 60
+
+    def drive(self, km: int):
+        if self.__petrol >= km:
+            self.__reading += km
+            self.__petrol -= km
+
+    def __str__(self):
+        return f"odometer reading {self.__reading} km, petrol remaining {self.__petrol} litres"
+
+if __name__ == "__main__":
+    car = Car()
+    print(car)
+    car.fill_up()
+    print(car)
+    car.drive(20)
+    print(car)
+    car.drive(50)
+    print(car)
+    car.drive(10)
+    print(car)
+    car.fill_up()
+    car.fill_up()
+    print(car)

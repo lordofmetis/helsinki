6eedc6a00648a9d744b39badaf3446d10f103fac
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-08 10:12:56 +0800

    Update files

--- part09-05_comparing_properties/src/comparing_properties.py
+++ part09-05_comparing_properties/src/comparing_properties.py
@@ -5,3 +5,23 @@ class RealProperty:
         self.rooms = rooms
         self.square_metres = square_metres
         self.price_per_sqm = price_per_sqm
+
+    def bigger(self, compared_to: "RealProperty"):
+        return self.square_metres > compared_to.square_metres
+
+    def price_difference(self, compared_to: "RealProperty"):
+        if self.bigger(compared_to):
+            return (self.price_per_sqm * self.square_metres) - (compared_to.price_per_sqm * compared_to.square_metres)
+        else:
+            return (compared_to.price_per_sqm * compared_to.square_metres) - (self.price_per_sqm * self.square_metres)
+    
+    def more_expensive(self, compared_to: "RealProperty"):
+        return (self.price_per_sqm * self.square_metres) > (compared_to.price_per_sqm * compared_to.square_metres)
+
+if __name__ == "__main__":
+    central_studio = RealProperty(1, 16, 5500)
+    downtown_two_bedroom = RealProperty(2, 38, 4200)
+    suburbs_three_bedroom = RealProperty(3, 78, 2500)
+
+    print(central_studio.more_expensive(downtown_two_bedroom))
+    print(suburbs_three_bedroom.more_expensive(downtown_two_bedroom))

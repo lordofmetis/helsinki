4cf3c9c598dfb4fef3ed6de4c540a4fc139dcd45
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-09 22:40:04 +0800

    Update files

--- part09-11_weather_station/src/weather_station.py
+++ part09-11_weather_station/src/weather_station.py
@@ -1,3 +1,35 @@
 # WRITE YOUR SOLUTION HERE:
+class WeatherStation:
+    def __init__(self, name: str):
+        self.__name = name
+        self.__list = []
+        self.__count = 0
+    
+    def add_observation(self, observation: str):
+        self.__list.append(observation)
+        self.__count += 1
+
+    def latest_observation(self):
+        if len(self.__list) != 0:
+            return self.__list[-1]
+        else:
+            return "" 
+
+    def number_of_observations(self):
+        return self.__count
+
+    def __str__(self):
+        return f"{self.__name}, {self.__count} observations"
 
 
+if __name__ == "__main__":
+    station = WeatherStation("Houston")
+    station.add_observation("Rain 10mm")
+    station.add_observation("Sunny")
+    print(station.latest_observation())
+
+    station.add_observation("Thunderstorm")
+    print(station.latest_observation())
+
+    print(station.number_of_observations())
+    print(station)
--- part09-14_list_helper/src/list_helper.py
+++ part09-14_list_helper/src/list_helper.py
@@ -1,1 +1,34 @@
 # WRITE YOUR SOLUTION HERE:
+class ListHelper:
+    def __init__(self):
+        pass
+
+    @classmethod
+    def greatest_frequency(cls, numbers: list):
+        temp = {}
+        for number in numbers:
+            if number not in temp:
+                temp[number] = 1
+            else:
+                temp[number] += 1
+
+        max_count = max(temp.values())
+        for number, count in temp.items():
+            if count == max_count:
+                return number
+
+    @classmethod
+    def doubles(cls, numbers: list):
+        temp = {}
+        for number in numbers:
+            if number not in temp:
+                temp[number] = 1
+            else:
+                temp[number] += 1
+
+        return len([number for number, count in temp.items() if count >= 2])
+
+if __name__ == "__main__":
+    numbers = [3, 2, 3, 2, 2, 3, 2, 2, 1, 1, 2]
+    print(ListHelper.greatest_frequency(numbers))
+    print(ListHelper.doubles(numbers))

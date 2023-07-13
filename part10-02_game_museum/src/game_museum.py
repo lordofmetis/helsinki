b9937537b9cb0ea7d8bda2c0f7f58736d37db262
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-13 16:53:48 +0800

    Update a file

--- part10-02_game_museum/src/game_museum.py
+++ part10-02_game_museum/src/game_museum.py
@@ -15,3 +15,25 @@ class GameWarehouse:
     def list_games(self):
         return self.__games
 
+class GameMuseum(GameWarehouse):
+    def __init(self):
+        super().__init__()
+    
+    def add_game(self, game: ComputerGame):
+        super().add_game(game)
+
+    def list_games(self):
+        real_list = super().list_games()
+        for real in real_list:
+            if real.year>= 1990:
+                real_list.remove(real)
+        return real_list
+
+
+if __name__ == "__main__":
+    museum = GameMuseum()
+    museum.add_game(ComputerGame("Pacman", "Namco", 1980))
+    museum.add_game(ComputerGame("GTA 2", "Rockstar", 1999))
+    museum.add_game(ComputerGame("Bubble Bobble", "Taito", 1986))
+    for game in museum.list_games():
+        print(game.name)

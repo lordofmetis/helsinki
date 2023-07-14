eadf846ecb9b6853a6a96ee490a35b4818e95e9a
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-14 14:12:25 +0800

    Update files

--- part10-07_money/src/money.py
+++ part10-07_money/src/money.py
@@ -1,8 +1,64 @@
 # TEE RATKAISUSI TÄHÄN:
 class Money:
     def __init__(self, euros: int, cents: int):
-        self.euros = euros
-        self.cents = cents
+        self._euros = euros
+        self._cents = cents
 
     def __str__(self):
-        return f"{self.euros}.{self.cents}"
+        return f"{self._euros}.{self._cents:0>2} eur"
+
+    def __eq__(self, another):
+        return (self._euros == another._euros) and (self._cents == another._cents)
+
+    def __ne__(self, another):
+        return (self._euros != another._euros) or (self._cents != another._cents)
+
+    def __lt__(self, another):
+        if self._euros < another._euros:
+            return True
+        elif self._euros == another._euros:
+            if self._cents < another._cents:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __ge__(self, another):
+        if self._euros > another._euros:
+            return True
+        elif self._euros == another._euros:
+            if self._cents > another._cents:
+                return True
+            else:
+                return False
+        else:
+            return False
+
+    def __add__(self, another):
+        if self._cents + another._cents >= 100:
+            return f"{self._euros + another._euros + 1}.{(self._cents + another._cents - 100):0>2} eur"
+        else:
+            return f"{self._euros + another._euros}.{(self._cents + another._cents):0>2} eur"
+
+    def __sub__(self, another):
+        if self.__lt__(another):
+            raise ValueError("a negative result is not allowed")
+        else:
+            if self._cents - another._cents < 0:
+                return f"{self._euros - another._euros - 1}.{(self._cents + 100 - another._cents):0>2} eur"
+            else:
+                return f"{self._euros - another._euros}.{(self._cents - another._cents):0>2} eur"
+
+
+if __name__ == "__main__":
+    e1 = Money(4, 5)
+    e2 = Money(2, 95)
+
+    e3 = e1 + e2
+    e4 = e1 - e2
+
+    print(e3)
+    print(e4)
+
+    e5 = e2-e1
--- part10-08_simple_date/src/simple_date.py
+++ part10-08_simple_date/src/simple_date.py
@@ -1,2 +1,60 @@
 # WRITE YOUR SOLUTION HERE:
+class SimpleDate:
+    def __init__(self, date:int, month:int, year:int):
+        self.date = date
+        self.month = month
+        self.year = year
 
+    def __str__(self):
+        return f"{self.date}.{self.month}.{self.year}"
+    
+    def __eq__(self, another):
+        return (self.date == another.date) and (self.month == another.month) and (self.year == another.year)
+
+    def __ne__(self, another):
+        return (self.date != another.date) or (self.month != another.month) or (self.year != another.year)
+
+    def __lt__(self, another):
+        if self.year < another.year:
+            return True
+        elif self.year == another.year:
+            if self.month < another.month:
+                return True
+            elif self.month == another.month:
+                if self.date < another.date:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+    def __gt__(self, another):
+        if self.year > another.year:
+            return True
+        elif self.year == another.year:
+            if self.month > another.month:
+                return True
+            elif self.month == another.month:
+                if self.date > another.date:
+                    return True
+                else:
+                    return False
+            else:
+                return False
+        else:
+            return False
+
+if __name__ == "__main__":
+    d1 = SimpleDate(4, 10, 2020)
+    d2 = SimpleDate(28, 12, 1985)
+    d3 = SimpleDate(28, 12, 1985)
+
+    print(d1)
+    print(d2)
+    print(d1 == d2)
+    print(d1 != d2)
+    print(d1 == d3)
+    print(d1 < d2)
+    print(d1 > d2)

f15032c19aaff3fb69b766449d2a86cb046333f3
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-14 23:34:53 +0800

    Add and update files

--- part11-06_begin_with_vowel/src/begin_with_vowel.py
+++ part11-06_begin_with_vowel/src/begin_with_vowel.py
@@ -1,1 +1,8 @@
 # WRITE YOUR SOLUTION HERE:
+def begin_with_vowel(words: list):
+    return [n for n in words if n.lower().startswith(("a", "e", "i", "o", "u"))]
+
+if __name__ == "__main__":
+    word_list = ["automobile","motorbike","Animal","cat","Dog","APPLE","orange"]
+    for vowelled in begin_with_vowel(word_list):
+        print(vowelled)
--- part11-07_lottery_numbers/src/lottery_numbers.py
+++ part11-07_lottery_numbers/src/lottery_numbers.py
@@ -1,1 +1,17 @@
 # WRITE YOUR SOLUTION HERE:
+class LotteryNumbers:
+    def __init__(self, weeks: int, correct_numbers: int):
+        self.weeks = weeks
+        self.correct_numbers = correct_numbers
+
+    def number_of_hits(self, my_numbers: list):
+        return sum([1 for number in my_numbers if number in self.correct_numbers])
+
+    def hits_in_place(self, my_numbers: list):
+        return [number if number in self.correct_numbers else -1 for number in my_numbers]
+
+if __name__ == "__main__":
+    week8 = LotteryNumbers(8, [1,2,3,10,20,30,33])
+    my_numbers = [1,4,7,10,11,20,30]
+
+    print(week8.hits_in_place(my_numbers))

43d3eff56c309bd65e83a27ae89294ea121b07e0
lordofmetis <47999593+lordofmetis@users.noreply.github.com>
2023-07-15 23:13:52 +0800

    Update files

--- part11-08_filter_forbidden/src/filter_forbidden.py
+++ part11-08_filter_forbidden/src/filter_forbidden.py
@@ -1,1 +1,9 @@
 # WRITE YOUR SOLUTION HERE:
+def filter_forbidden(string: str, forbidden: str):
+    return "".join([s for s in string if s not in forbidden])
+
+
+if __name__ == "__main__":
+    sentence = "Once! upon, a time: there was a python!??!?!"
+    filtered = filter_forbidden(sentence, "!?:,.")
+    print(filtered)
--- part11-12_most_common_words/src/most_common_words.py
+++ part11-12_most_common_words/src/most_common_words.py
@@ -1,1 +1,18 @@
 # WRITE YOUR SOLUTION HERE:
+import string
+
+def most_common_words(filename: str, lower_limit: int):
+    temp = []
+
+    with open(filename) as file:
+        for line in file:
+            parts = line.split()
+            for part in parts:
+                if part[-1] in string.punctuation:    
+                    part = part[:-1]
+                temp.append(part)
+
+    return {word: temp.count(word) for word in temp if temp.count(word) >= lower_limit}
+
+if __name__ == "__main__":
+    most_common_words("comprehensions.txt", 3)
